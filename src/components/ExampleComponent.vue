a5278b9ed2995af4b326fbcbf1627212e97ad1ce
jdavid2019 <joseph2019d@gmail.com>
2021-05-03 00:45:17 -0500

    first commit

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1>{{ all }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+name: "ExampleComponent.vue",
+  data(){
+    return {
+      all: 'Hola con todos'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Example.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <ExampleComponent />
+</div>
+</template>
+
+<script>
+import ExampleComponent from "@/components/ExampleComponent";
+export default {
+  name: "Example.vue",
+  components: {
+    ExampleComponent
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
